3b4c61996d1436ffd7d7cc5764ea2d4ef4678826
pja1982 <pja1982@gmail.com>
2014-10-06 01:04:37 +1100

    Initial code
    Started coding my Reactor control program

---
+++ Reactor.lua
@@ -0,0 +1,17 @@
+local component = require("component")
+local event = require("event")
+local fs = require("filesystem")
+local keyboard = require("keyboard")
+local shell = require("shell")
+local term = require("term")
+local text = require("text")
+local unicode = require("unicode")
+local sides = require("sides")
+local colors=require("colors")
+
+local gpu=component.gpu
+local br = component.br_reactor
+local rs = component.redstone
+
+----
+
