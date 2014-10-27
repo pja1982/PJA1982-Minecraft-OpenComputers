5727dd853eacd802c08c572c9b47980fe3211470
pja1982 <pja1982@gmail.com>
2014-10-27 23:41:56 +1100

    GUI Improvements and Sleep Mode
    Added Some Basic Reactor Info and Turn Off Reactor at 95% stored RF and
    turn on at 10%.

--- Reactor.lua
+++ Reactor.lua
@@ -31,20 +31,34 @@ local function KeyPress(opt)
 end
 
 local function printXY(row, col,  ...)
-	term.setCursor(col, row)
-	print((...))
+term.setCursor(col, row)
+print((...))
 end
+
 ----
 
 
 while runmode do 
 term.clear()
 setrod = 0
-	energy = br.getEnergyStored() / 100000
-energyrod = math.abs (math.ceil (energy * rods))
-	printXY(1, 1, energy)
-	printXY(2, 1, energyrod)
+energy = br.getEnergyStored() / 100000
+printXY(1, 1, "Reactor State")
+printXY(2, 1, "Reactor Charge Level          %")
+printXY(3, 1, "Control Rod Level             %")
+printXY(4, 1, "Fuel Level                    mB")
+printXY(5, 1, "Waste Level                   mB")
+printXY(6, 1, "Fuel Cap                      mB")
 
+printXY(8, 1, "Energy Output                 RF")
+
+printXY(10, 1, "Coolant Type")
+printXY(11, 1, "Coolant Level                 mB")
+printXY(12, 1, "Coolant Level Max             mB")
+
+
+if ((br.getActive()) == true) then
+printXY(1, 25, "Online")
+energyrod = math.abs (math.ceil (energy * rods))
 while (setrod < (rods - 1)) do
   if (energyrod - (100 * (setrod + 1)) > 99) then
     br.setControlRodLevel(setrod,100)
@@ -55,6 +69,33 @@ while (setrod < (rods - 1)) do
   end
   setrod = (setrod + 1) 
 end
+else
+printXY(1, 25, "Offline")
+
+end
+
+printXY(2, 25, math.floor (energy))
+printXY(3, 25, math.floor ((energyrod / rods)))
+printXY(4, 25, math.floor (br.getFuelAmount()))
+printXY(5, 25, math.floor (br.getWasteAmount()))
+printXY(6, 25, math.floor (br.getFuelAmountMax()))
+
+printXY(8, 25, math.floor (br.getEnergyProducedLastTick()))
+
+printXY(10, 25,(br.getCoolantType()))
+printXY(11, 25, math.floor (br.getCoolantAmount()))
+printXY(12, 25, math.floor (br.getCoolantAmountMax()))
+
+if ((br.getEnergyStored() / 100000) > 95) then
+	br.setActive(false)
+printXY(1, 25, "Online")
+elseif ((br.getEnergyStored() / 100000) < 10) then
+	br.setActive(true)
+printXY(1, 25, "Offline")
+
+end
+
+
 
   local event, address, arg1, arg2, arg3 = event.pull(1)
   if type(address) == "string" and component.isPrimary(address) then
